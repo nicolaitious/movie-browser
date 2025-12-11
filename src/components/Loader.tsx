307f21087906216d8fa25784b05bf69e55494f3b
nicolaitious <nicomuscio96@hotmail.com>
2025-12-11 14:59:01 -0300

    .

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,3 +1,3 @@
 export default function Loader({ size = "normal" }: { size?: "normal" | "small" }) {
-    return <div style={{ paddingTop: size === "small" ? 8 : '25vh', textAlign: 'center' }}><img src={"/src/assets/loader.svg"} /></div>;
+    return <div style={{ paddingTop: size === "small" ? 8 : '25vh', textAlign: 'center' }}><img src={"/loader.svg"} /></div>;
 }
